23bbf0032712ec778d846650d9ddeda5076d01a2
ohadk <ohadkachlon@gmail.com>
2023-01-15 22:47:43 +0200

    1

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import Sidenav from'./components/Sidenav'
+function App() {
+
+  return (
+    <div>
+      <Sidenav/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Sidenav.jsx
@@ -0,0 +1,31 @@
+import React,{useState} from 'react'
+import {AiOutlineHome, AiOutlineMenu} from 'react-icons/ai'
+
+const Sidenav =()=> {
+  const [nav,setNav]= useState(false)
+  const handleNav=()=>{
+   setNav(!nav); 
+  };
+  return (
+    <div>
+      <AiOutlineMenu onClick={handleNav} className='absolute top-4 right-4 z-[99] md:hidden'/>
+      {
+        nav ? (
+         <div className='fixed w-full h-screen bg-white/90 flex flex=col justify-center items-center z-30'>
+          <a href="#main">
+            <AiOutlineHome size={20}/>
+            <span className='pl-4'>Home</span>
+          </a>
+         </div>
+        )
+        :(
+          <div>
+          
+          </div>
+        ) 
+      }
+    </div>
+  )
+}
+
+export default Sidenav
