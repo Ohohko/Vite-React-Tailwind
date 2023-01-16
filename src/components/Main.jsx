1dcb4b06b5a4fc3756e2110abf7dd1ee450c8d6f
ohadk <ohadkachlon@gmail.com>
2023-01-16 11:59:12 +0200

    add react-type-animation

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,7 +3,13 @@ import React from 'react'
 const Main = () => {
   return (
     <div id='main'>
-   <img className='w-full h-screen object-cover object-left scale-x-[-1]' src="/src/assets/main-img.jpg" alt="background"/>
+    <img className='w-full h-screen object-cover object-left scale-x-[-1]' src="/src/assets/main-img.jpg" alt="background"/>
+     <div className='w-full h-screen absolute top-0 left-0 bg-white/50'>
+     <div>
+        <h1>I'm Ohad Kahlon</h1>
+        <h2>I'm a </h2>
+     </div>   
+     </div>
     </div>
   )
 }
--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -9,7 +9,7 @@ const Sidenav =()=> {
   };
   return (
     <div>
-      <AiOutlineMenu onClick={handleNav} className='absolute top-4 right-4 z-[99] md:hidden'/>
+      <AiOutlineMenu size={35} onClick={handleNav} className='absolute top-4 right-4 z-[99] md:hidden'/>
       {
         nav ? (
          <div className='fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center z-30'>
