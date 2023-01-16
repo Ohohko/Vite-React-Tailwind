d7c921ab4f80118cd29954179e02f96ed4212c19
ohadk <ohadkachlon@gmail.com>
2023-01-16 08:23:49 +0200

    add navbar

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -16,6 +16,22 @@ const Sidenav =()=> {
             <AiOutlineHome size={20}/>
             <span className="pl-4">Home</span>
           </a>
+          <a href="#main" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-100 ease-in duration-200'>
+            <AiOutlineHome size={20}/>
+            <span className="pl-4">Home</span>
+          </a>
+          <a href="#main" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-100 ease-in duration-200'>
+            <AiOutlineHome size={20}/>
+            <span className="pl-4">Home</span>
+          </a>
+          <a href="#main" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-100 ease-in duration-200'>
+            <AiOutlineHome size={20}/>
+            <span className="pl-4">Home</span>
+          </a>
+          <a href="#main" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-100 ease-in duration-200'>
+            <AiOutlineHome size={20}/>
+            <span className="pl-4">Home</span>
+          </a>
          </div>
         )
         :(
