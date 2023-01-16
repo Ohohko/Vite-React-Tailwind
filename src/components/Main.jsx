9e16e1fdc584ca764faab0abf6d04e597aebb9ed
ohadk <ohadkachlon@gmail.com>
2023-01-16 12:19:39 +0200

    edit type animation

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { TypeAnimation } from 'react-type-animation'
 
 const Main = () => {
   return (
@@ -7,7 +8,25 @@ const Main = () => {
      <div className='w-full h-screen absolute top-0 left-0 bg-white/50'>
      <div>
         <h1>I'm Ohad Kahlon</h1>
-        <h2>I'm a </h2>
+        <h2>Welcome to my React website
+        <TypeAnimation
+      sequence={[
+        'Java Script', 
+        1500, // Waits 1s
+        'React', // Deletes 'One' and types 'Two'
+        2000, // Waits 2s
+        'Deploy to Aws.',
+        2000, // Types 'Three' without deleting 'Two'
+        () => {
+          console.log('Done typing!'); // Place optional callbacks anywhere in the array
+        }
+      ]}
+      wrapper="div"
+      cursor={true}
+      repeat={Infinity}
+      style={{ fontSize: '2em' }}
+    />   
+        </h2>
      </div>   
      </div>
     </div>
