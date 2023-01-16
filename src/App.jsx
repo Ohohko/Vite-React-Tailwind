90998436bf81a3d065bf3aabd1f97845695f5368
ohadk <ohadkachlon@gmail.com>
2023-01-16 11:44:56 +0200

    start main and img

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import Sidenav from'./components/Sidenav'
+import Main from './components/Main'
 function App() {
 
   return (
     <div>
       <Sidenav/>
+      <Main/>
     </div>
   )
 }
---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div id='main'>
+   <img className='w-full h-screen object-cover object-left scale-x-[-1]' src="/src/assets/main-img.jpg" alt="background"/>
+    </div>
+  )
+}
+
+export default Main
