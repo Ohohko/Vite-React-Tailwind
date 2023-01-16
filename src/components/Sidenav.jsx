2250a58df05e26a914687caef352a4c2fa1a416e
ohadk <ohadkachlon@gmail.com>
2023-01-16 08:09:27 +0200

    /

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -11,12 +11,11 @@ const Sidenav =()=> {
       <AiOutlineMenu onClick={handleNav} className='absolute top-4 right-4 z-[99] md:hidden'/>
       {
         nav ? (
-         <div className='fixed w-full h-screen bg-white/90 flex flex=col justify-center items-center z-30'>
-          <a href="#main">
+         <div className='fixed w-full h-sceen bg-white/90 flex flex=col justify-center items-center z-30'>
+          <a href="#main" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-100 ease-in duration-200'>
             <AiOutlineHome size={20}/>
-            <span className='pl-4'>Home</span>
+            <span className="pl-4">Home</span>
           </a>
-          <p>dcdcds</p>
          </div>
         )
         :(
