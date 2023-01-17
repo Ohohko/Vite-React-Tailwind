01cfa0255e28c895be9293c51598346a919f40a5
ohadk <ohadkachlon@gmail.com>
2023-01-17 04:47:15 +0200

    color icons

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,9 +8,9 @@ const Main = () => {
     <img className='w-full h-screen object-cover object-left scale-x-[-1]' src="/src/assets/main-img.jpg" alt="background"/>
      
      <div className='w-full h-screen absolute top-0 left-0 bg-white/50'>
-     <div className='max-w-[700px] m-auto h-full flex flex-col justify-center lg:items-start items-center' >
+     <div className='max-w-[700px] m-auto h-full flex flex-col justify-center lg:items-center items-center' >
          <h1 className='sm:text-5xl text-4xl font-bold text-text-5xl extrabold text-transparent bg-clip-text bg-gradient-to-br from-pink-400 to-red-600'>I'm Ohad Kahlon</h1>
-        <h2>Welcome to my React Website
+        <h2 className='text-2xl pt-4 text-center text-cyan-800 '>Welcome to my React Website
         <TypeAnimation
       sequence={[
         'Java Script', 
@@ -26,11 +26,11 @@ const Main = () => {
       style={{ fontSize: '1em' }}
     />   
         </h2>
-        <div>
-            <FaTwitter    className='cursor-pointer' size={25} />
-            <FaFacebookF  className='cursor-pointer' size={25} />
-            <FaInstagram  className='cursor-pointer' size={25} />
-            <FaLinkedinIn className='cursor-pointer' size={25} />
+        <div className='flex justify-around pt-7 max-[200px] w-full'>
+            <FaTwitter    className='cursor-pointer text-cyan-800' size={25}  />
+            <FaFacebookF  className='cursor-pointer text-cyan-800' size={25}  />
+            <FaInstagram  className='cursor-pointer text-cyan-800' size={25}  />
+            <FaLinkedinIn className='cursor-pointer text-cyan-800' size={25}  />
             
         </div>
      </div>   
--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -9,27 +9,27 @@ const Sidenav =()=> {
   };
   return (
     <div>
-      <AiOutlineMenu size={35} onClick={handleNav} className='absolute top-4 right-4 z-[99] md:hidden'/>
+      <AiOutlineMenu size={35} onClick={handleNav} className='absolute top-4 right-4 text-cyan-800 z-[99] md:hidden'/>
       {
         nav ? (
-         <div className='fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center z-30'>
-          <a href="#main" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-100 ease-in duration-200'>
+         <div className='fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center z-30 text-cyan-800'>
+          <a href="#main" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-100 ease-in duration-200 text-cyan-800'>
             <AiOutlineHome size={20}/>
             <span className="pl-4">Home</span>
           </a>
-          <a href="#work" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-100 ease-in duration-200'>
+          <a href="#work" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-100 ease-in duration-200 text-cyan-800'>
             <AiOutlineFundProjectionScreen size={20}/>
             <span className="pl-4">Work</span>
           </a>
-          <a href="#project" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-100 ease-in duration-200'>
+          <a href="#project" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-100 ease-in duration-200 text-cyan-800'>
             <GrWorkshop size ={20}/>
             <span className="pl-4">Project</span>
           </a>
-          <a href="#main" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-100 ease-in duration-200'>
+          <a href="#main" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-100 ease-in duration-200 text-cyan-800'>
             <SiAboutdotme size={20}/>
             <span className="pl-4">Resume</span>
           </a>
-          <a href="#contact" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-100 ease-in duration-200'>
+          <a href="#contact" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-100 ease-in duration-200 text-cyan-800'>
             <SiMaildotru size={20}/>
             <span className="pl-4">Contact</span>
           </a>
@@ -40,19 +40,19 @@ const Sidenav =()=> {
       )}
      <div className='md:block hidden fixed top-[25%] z-10'>
         <div className='flex flex-col'>
-          <a href='#main' className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+          <a href='#main' className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300 text-cyan-800'>
           <AiOutlineHome size={25}/>            
           </a>
-          <a href='#work' className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+          <a href='#work' className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300 text-cyan-800'>
           <AiOutlineFundProjectionScreen  size={25}/>            
           </a>
-          <a href='#project' className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+          <a href='#project' className='text-cyan-800 rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
           <GrWorkshop size={25}/>            
           </a>
-          <a href='#main' className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+          <a href='#main' className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300 text-cyan-800'>
           <SiAboutdotme size={25}/>            
           </a>
-          <a href='#contact' className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+          <a href='#contact' className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300 text-cyan-800'>
           <SiMaildotru size={25}/>            
           </a>
         </div>
